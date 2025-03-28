8cbeb1c2dcd76e4b9ad634186d4a99f3609fe2a8
Dear0001 <dalensophea34@gmail.com>
2025-03-29 01:29:44 +0700

    console log profile in table users seo again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
         title: "Krorya dashboard web",
         description:
             "krorya is a web application for selling products the same as advertising about culture of khmer.",
-        images: "http://localhost:8080/api/v1/fileView/16e8cb88-b356-4f65-80b8-1f84709d5a6c.png",
+        images: "http://krorya-api.up.railway.app/api/v1/fileView/3a0e04d1-b004-455d-95c7-4acf283a8ef1.png",
     },
 };
 
